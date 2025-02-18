663b1054a308c839ef52f756bf3ec221d387cbe6
kelseyy9 <131050246+kelseyy9@users.noreply.github.com>
2025-02-18 18:30:01 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember (){
+    var member = {
+        name: "Riki",
+        age: 20,
+        skills: ["JavaScript", "HTML", "CSS", "NodeJS", "ReactJS"]
+    }
+    return member
+}
